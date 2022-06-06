5d0e94ae4e3e03d273f0f0e4d8c565b7601ca93e
mac <aliaksejbaluhtin24@gmail.com>
2022-06-07 01:25:04 +0200

    Initial Commit

---
+++ TestSearchPhotos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestSearchPhotos
+//
+//  Created by Alex Balukhtsin on 7.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
